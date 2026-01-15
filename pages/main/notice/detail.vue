7809b29b7f797b6c90a5f78928288ae706634bc1
wupeng <1421386897@qq.com>
2026-01-15 10:58:07 +0800

    feat：通知公共列表和详情调整

--- pages/main/notice/detail.vue
+++ pages/main/notice/detail.vue
@@ -1,72 +1,91 @@
 <template>
 	<view class="detail-wrapper">
-		<view class="content" v-if="strings != ''">
-			<view class="title" >{{strings.name}}</view>
-			<rich-text class="text" :nodes="strings.content"></rich-text>
-			<view class="time" >{{strings.c_time}}</view>
+		<view class="content" v-if="detail">
+			<view class="title">{{detail.name}}</view>
+			<rich-text class="text" :nodes="detail.content"></rich-text>
+			<view class="time">{{detail.c_time}}</view>
 		</view>
+		<view v-else class="empty">内容不存在</view>
 	</view>
 </template>
 
 <script>
-	export default{
+	export default {
 		data() {
 			return {
-				strings: '',
-				id:''
+				detail: null,
 			}
 		},
-		methods:{
-			getCircularInfo(){
-				let data ={
-					id : this.id
-				}
-				this.$api.circularInfo(data,res =>{
-					this.strings = res.data
+		methods: {
+			getCircularInfo(id) {
+				this.$api.circularInfo({
+					id: id
+				}, res => {
+					if (res.code === 1 && res.data) {
+						this.detail = res.data
+					}
 				})
 			}
 		},
-		onLoad() {
-			this.id = this.$Route.query.id;
-			this.getCircularInfo();
+		onLoad(options) {
+			const id = this.$Route.query.id;
+			if (id) {
+				this.getCircularInfo(id)
+			} else {
+				uni.showToast({
+					title: '参数错误',
+					icon: 'error'
+				})
+			}
 		}
 	}
 </script>
 
 <style>
-.detail-wrapper {
-	min-height: 100vh;
-	background-color: #FAFAFA;
-	padding: 30upx 24upx;
-	box-sizing: border-box;
-}
-.detail-wrapper .content {
-	background-color: #fff;
-	box-shadow: 0 0 6upx rgba(0, 0, 0, 0.05);
-	padding: 0;
-	border-radius: 10upx;
-}
-.detail-wrapper .content .title {
-	font-size: 30upx;
-	line-height: 44upx;
-	font-weight: 600;
-	color: #333;
-	padding: 30upx;
-	text-align: center;
-	border-bottom: 1px solid #ebebeb;
-}
-.detail-wrapper .content .text {
-	padding: 30upx 30upx 0 30upx;
-	font-size: 30upx;
-	line-height: 42upx;
-	text-align: justify;
-	color: #333;
-}
-.detail-wrapper .content .time {
-	font-size: 26upx;
-	color: #666;
-	line-height: 36upx;
-	padding: 30upx;
-	text-align: right;
-}
+	.detail-wrapper {
+		min-height: 100vh;
+		background-color: #FAFAFA;
+		padding: 30upx 24upx;
+		box-sizing: border-box;
+	}
+
+	.detail-wrapper .content {
+		background-color: #fff;
+		box-shadow: 0 0 6upx rgba(0, 0, 0, 0.05);
+		padding: 0;
+		border-radius: 10upx;
+	}
+
+	.detail-wrapper .content .title {
+		font-size: 30upx;
+		line-height: 44upx;
+		font-weight: 600;
+		color: #333;
+		padding: 30upx;
+		text-align: center;
+		border-bottom: 1px solid #ebebeb;
+	}
+
+	.detail-wrapper .content .text {
+		padding: 30upx 30upx 0 30upx;
+		font-size: 30upx;
+		line-height: 42upx;
+		text-align: justify;
+		color: #333;
+	}
+
+	.detail-wrapper .content .time {
+		font-size: 26upx;
+		color: #666;
+		line-height: 36upx;
+		padding: 30upx;
+		text-align: right;
+	}
+	.detail-wrapper .empty {
+		font-size: 28upx;
+		color: #666;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
--- pages/main/notice/notice.vue
+++ pages/main/notice/notice.vue
@@ -4,14 +4,14 @@
 			<image src="/static/images/message.png" mode="aspectFit"></image>
 			<view class="title">{{ item.name }}</view>
 			<view class="content">
-				<rich-text class="text" :nodes="item.content"></rich-text>
+				<rich-text class="text" :nodes="getPreview(item.content)"></rich-text>
 			</view>
 			<view class="bot">
 				<!-- <text class="jituan">阳光大地物业集团</text> -->
 				<text class="time">{{ item.ctime }}</text>
 			</view>
 		</view>
-<!-- 		<view class="empty" v-if="list.length == 0">
+		<!-- <view class="empty" v-if="list.length == 0">
 			<image src="/static/images/inform-mask.png" mode=""></image>
 			<view class="tip">当前社区暂无公告通知</view>
 			<button class="btn" plain @click="toMyhouse">更换社区</button>
@@ -20,60 +20,76 @@
 	</view>
 </template>
 <script>
-export default {
-	data() {
-		return {
-			list: [],
-			page: 1,
-			limit: 10000
-		};
-	},
-	onReachBottom: function() {
-		// console.log('我触底了要加载数据了: ' + JSON.stringify('我触底了要加载数据了'));
-	},
-	methods: {
-		detail(item) {
-			this.$Router.push({ name: 'notice-detail', params: { id: item.id } });
-		},
-		getList() {
-			let data = {
-				page: this.page,
-				limit: this.limit
+	export default {
+		data() {
+			return {
+				list: [],
 			};
-			this.$api.circularList(data, res => {
-				this.list = res.data;
-				this.list.forEach(res => {
-					if (res.content.length > 300) {
-						res.content = res.content.substring(0, 300);
+		},
+		onReachBottom: function() {
+			// console.log('我触底了要加载数据了: ' + JSON.stringify('我触底了要加载数据了'));
+		},
+		methods: {
+			detail(item) {
+				this.$Router.push({
+					name: 'notice-detail',
+					params: {
+						id: item.id
 					}
 				});
-			});
+			},
+			getList() {
+				this.$api.circularList({}, res => {
+					if (res.code === 1) {
+						this.list = res.data || [];
+					}
+				});
+			},
+			// 跳转我的房产页面
+			toMyhouse() {
+				this.$Router.push({
+					name: 'my-house'
+				});
+			},
+			// 获取预览内容
+			getPreview(html) {
+				if (!html) return ''
+				try {
+					const text = html
+						.replace(/<\/?[^>]+(>|$)/g, ' ') // 清除所有标签（含不完整）
+						.replace(/\s+/g, ' ') // 合并多余空格
+						.trim() // 去除首尾空格
+
+					return text.length > 300 ? text.substring(0, 300) + '...' : text
+				} catch (e) {
+					console.error('解析富文本失败:', e)
+					return ''
+				}
+			}
 		},
-		// 跳转我的房产页面
-		toMyhouse() {
-			this.$Router.push({ name: 'my-house'});
+		onLoad() {
+			this.getList();
 		}
-	},
-	onLoad() {
-		this.getList();
-	}
-};
+	};
 </script>
 
 <style lang="scss">
-#notice {
-	min-height: 100vh;
-	background-color: #fafafa;
-	padding: 30upx 24upx;
-	box-sizing: border-box;
-}
+	#notice {
+		min-height: 100vh;
+		background-color: #fafafa;
+		padding: 30upx 24upx;
+		box-sizing: border-box;
+	}
+
 	.empty {
 		text-align: center;
+
 		image {
 			width: 250upx;
 			height: 220upx;
 			margin-top: 250upx;
 		}
+
 		.tip {
 			font-size: 32upx;
 			line-height: 44upx;
@@ -81,72 +97,82 @@ export default {
 			margin-top: 20upx;
 			text-align: center;
 		}
+
 		.btn {
 			font-size: 28upx;
 			color: #fff;
 			line-height: 40upx;
-			padding: 10upx 44upx!important;
+			padding: 10upx 44upx !important;
 			background-color: #ffcf5a;
 			border-radius: 4upx;
 			margin-top: 60upx;
 			display: inline-block;
-			border: none!important;
+			border: none !important;
 		}
+
 		.btn:active {
 			opacity: 0.8;
 		}
 	}
-.notice-item {
-	background-color: #fff;
-	box-shadow: 0 0 8upx rgba(0, 0, 0, 0.05);
-	padding: 0 30upx;
-	position: relative;
-	border-radius: 10upx;
-	margin-bottom: 30upx;
-}
-.notice-item image {
-	width: 40upx;
-	height: 34upx;
-	position: absolute;
-	top: 36upx;
-	left: 32upx;
-}
-.notice-item .title {
-	font-size: 30upx;
-	color: #333;
-	line-height: 44upx;
-	padding: 30upx 0 30upx 60upx;
-	font-weight: 600;
-}
-.notice-item .content {
-	background-color: #fafafa;
-	padding: 30upx;
-}
-.notice-item .content .text {
-	font-size: 28upx;
-	line-height: 40upx;
-	color: #999;
-	text-align: justify;
-	overflow:hidden; 
-	text-overflow:ellipsis;
-	display:-webkit-box;
-	-webkit-box-orient:vertical;
-	-webkit-line-clamp:4;
-	word-wrap: break-word;
-}
-.notice-item .bot {
-	padding: 30upx 0;
-	overflow: hidden;
-}
-.notice-item .jituan,
-.notice-item .time {
-	font-size: 26upx;
-	line-height: 36upx;
-	color: #666;
-}
-.notice-item .time {
-	float: right;
-}
-.uni-empty {
-}
+
+	.notice-item {
+		background-color: #fff;
+		box-shadow: 0 0 8upx rgba(0, 0, 0, 0.05);
+		padding: 0 30upx;
+		position: relative;
+		border-radius: 10upx;
+		margin-bottom: 30upx;
+	}
+
+	.notice-item image {
+		width: 40upx;
+		height: 34upx;
+		position: absolute;
+		top: 36upx;
+		left: 32upx;
+	}
+
+	.notice-item .title {
+		font-size: 30upx;
+		color: #333;
+		line-height: 44upx;
+		padding: 30upx 0 30upx 60upx;
+		font-weight: 600;
+	}
+
+	.notice-item .content {
+		background-color: #fafafa;
+		padding: 30upx;
+	}
+
+	.notice-item .content .text {
+		font-size: 28upx;
+		line-height: 40upx;
+		color: #999;
+		text-align: justify;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 4;
+		word-wrap: break-word;
+	}
+
+	.notice-item .bot {
+		padding: 30upx 0;
+		overflow: hidden;
+	}
+
+	.notice-item .jituan,
+	.notice-item .time {
+		font-size: 26upx;
+		line-height: 36upx;
+		color: #666;
+	}
+
+	.notice-item .time {
+		float: right;
+	}
+
+	.uni-empty {}
 </style>
